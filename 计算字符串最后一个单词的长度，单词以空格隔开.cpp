a8890644b660e89724a630a12438fa3f4a390038
林 李 <137762376@qq.com>
2020-09-04 21:43:59 +0800

    1

---
+++ 计算字符串最后一个单词的长度，单词以空格隔开.cpp
@@ -0,0 +1,27 @@
+////计算字符串最后一个单词的长度，单词以空格隔开。
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    string s;
+//    getline(cin,s);
+//    if (s.size() <= 1)
+//    {
+//        cout << s.size();
+//        return 0;
+//    }
+//    int count = 0;
+//    for (int i = 0; i < s.size(); i++)
+//    {
+//        count++;
+//        if (s[i] == ' ')
+//        {
+//            count = 0;
+//        }
+//    }
+//    cout << count << endl;
+//    return 0;
+//}
